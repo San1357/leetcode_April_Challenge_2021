30211c07b28da4fcfbb648bbd66c77184c3a1501
San Ayush <41052723+San1357@users.noreply.github.com>
2021-04-02 17:16:25 +0530

    Update Ones_and_Zeroes.py

--- Ones_and_Zeroes.py
+++ Ones_and_Zeroes.py
@@ -16,3 +16,26 @@ class Solution:
         not_take_curr_str = self.helper(strs, m, n, idx + 1)
         return max(take_curr_str, not_take_curr_str)
         
+        
+        #    or
+        
+        
+ class Solution(object):
+    def findMaxForm(self, strs, m, n):
+        """
+        :type strs: List[str]
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        table = {k:[[0]*(n+1) for _ in range(m+1)] for k in range(len(strs)+1)}
+        for i in range(1, len(strs)+1):
+            zeroes, ones = strs[i-1].count('0'), strs[i-1].count('1')
+            for j in range(m+1):
+                for k in range(n+1):
+                    if j >= zeroes and k >= ones:
+                        table[i][j][k] = max(table[i-1][j][k], 1+table[i-1][j-zeroes][k-ones])
+                    else:
+                        table[i][j][k] = table[i-1][j][k]
+        return table[len(strs)][m][n]
+
