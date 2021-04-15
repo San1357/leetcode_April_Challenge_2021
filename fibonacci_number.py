cc163f5e7e21f0df60cdf87950556f9222b908d6
San Ayush <41052723+San1357@users.noreply.github.com>
2021-04-15 16:21:04 +0530

    Create fibonacci_number.py

---
+++ fibonacci_number.py
@@ -0,0 +1,13 @@
+'''Problem : fibonacci number '''
+
+#code :
+
+class Solution:
+    def fib(self, N: int) -> int:
+        dp = [0 for x in range(N+1)]
+        dp[0] = 0
+        if N>=1:
+            dp[1] = 1
+            for x in range(2,N+1):
+                dp[x] = dp[x-1]+dp[x-2]
+        return dp[-1]
